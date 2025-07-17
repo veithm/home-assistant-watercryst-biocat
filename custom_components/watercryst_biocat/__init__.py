d5dd3e7f11931204423bfe358c973d59c1f43af3
Martin Veith <3490591+veithm@users.noreply.github.com>
2025-07-17 20:30:58 +0200

    Update __init__.py

--- custom_components/watercryst_biocat/__init__.py
+++ custom_components/watercryst_biocat/__init__.py
@@ -1,12 +1,24 @@
-from homeassistant.core import HomeAssistant
 from .const import DOMAIN
+from .coordinator import WatercrystDataUpdateCoordinator
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    hass.data.setdefault(DOMAIN, {})
+async def async_setup(hass, config):
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry):
-    return await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+async def async_setup_entry(hass, entry):
+    api_key = entry.data["api_key"]
+    scan_interval = entry.data["scan_interval"]
+
+    coordinator = WatercrystDataUpdateCoordinator(hass, api_key, scan_interval)
+    await coordinator.async_refresh()
+
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
 
 async def async_unload_entry(hass, entry):
     unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
